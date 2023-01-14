90bc66e3d5f0ada7f943eb467c17f7ddd98cc711
Shashank <mishra.shashankofficial06@gmail.com>
2023-01-14 13:32:03 +0530

    Added: Welcome message

---
+++ src/com/day22assignment/AddressBookMain.java
@@ -0,0 +1,14 @@
+package com.day22assignment;
+
+/**
+ * @author Shashank
+ */
+
+/**
+ * AddressBookMain class, inside this we have main method
+ */
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("\nWelcome to Address Book Program");
+    }
+}
